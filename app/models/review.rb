b653c3caba3948d93e60c3dc789319f2d042bd63
Anders <kiss7medical@gmail.com>
2019-03-06 14:38:11 +0100

    numericality added to review.rb model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,8 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
+
+  # https://guides.rubyonrails.org/active_record_validations.html#numericality
+  # 2.8 numericality
   validates :rating, presence: true, inclusion: { in: (0..5)}, numericality: { only_integer: true }
 end
